b3213e3c9c3ef83771c9cfa864fa587c32ce5bd7
alxmart <alxmartinez@gmail.com>
2024-05-11 11:30:19 -0300

    Creating new activity AdicionarTarefaActivity

--- app/src/main/java/com/luizafmartinez/m23_app_todolist/AdicionarTarefaActivity.kt
+++ app/src/main/java/com/luizafmartinez/m23_app_todolist/AdicionarTarefaActivity.kt
@@ -5,25 +5,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.luizafmartinez.m23_app_todolist.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
-    private val binding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
-
+class AdicionarTarefaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-
-        binding.fab.setOnClickListener {
-            val intent = Intent(this, Ad )
-        }
-
-
-
-
-
     }
 }
