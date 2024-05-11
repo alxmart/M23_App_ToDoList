c47afec005be1ee5170fc560e0eff49f4f4e67a7
alxmart <alxmartinez@gmail.com>
2024-05-11 19:42:35 -0300

    Code to delete tasks. (fun confirmarExclusao)

--- app/src/main/java/com/luizafmartinez/m23_app_todolist/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/m23_app_todolist/MainActivity.kt
@@ -3,6 +3,8 @@ package com.luizafmartinez.m23_app_todolist
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.luizafmartinez.m23_app_todolist.adapter.TarefaAdapter
@@ -32,11 +34,48 @@ class MainActivity : AppCompatActivity() {
         }
 
         // RecyclerView
-        tarefaAdapter = TarefaAdapter()
+        tarefaAdapter = TarefaAdapter(
+            { id -> confirmarExclusao(id)
+
+            }
+        )
 
         binding.rvTarefas.adapter = tarefaAdapter
 
         binding.rvTarefas.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun confirmarExclusao(id: Int) {
+
+        val alertBuilder = AlertDialog.Builder(this)
+
+        alertBuilder.setTitle("Confirmar Exclusão")
+
+        alertBuilder.setMessage("Deseja realmente excluir a tarefa?")
+
+        alertBuilder.setPositiveButton("Sim") { _, _ ->
+            val tarefaDAO = TarefaDAO(this)
+            if (tarefaDAO.remover(id)) {
+                Toast.makeText(
+                    this,
+                    "Sucesso ao remover tarefa",
+                    Toast.LENGTH_SHORT
+                ).show()
+                atualizarListaTarefas()
+        } else {
+                Toast.makeText(
+                    this,
+                    "Erro ao remover tarefa",
+                    Toast.LENGTH_SHORT
+                ).show()
+        }
+    }
+
+        alertBuilder.setNegativeButton("Não") { _, _ ->
+
+        }
+
+        alertBuilder.create().show()
     }
 
     private fun atualizarListaTarefas() {
--- app/src/main/java/com/luizafmartinez/m23_app_todolist/adapter/TarefaAdapter.kt
+++ app/src/main/java/com/luizafmartinez/m23_app_todolist/adapter/TarefaAdapter.kt
@@ -6,7 +6,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.luizafmartinez.m23_app_todolist.databinding.ItemTarefaBinding
 import com.luizafmartinez.m23_app_todolist.model.Tarefa
 
-class TarefaAdapter() : RecyclerView.Adapter<TarefaAdapter.TarefaViewHolder>() {
+class TarefaAdapter(
+    val onClickExcluir: (Int) -> Unit
+) : RecyclerView.Adapter<TarefaAdapter.TarefaViewHolder>() {
 
     private var listaTarefas: List<Tarefa> = emptyList()
 
@@ -27,6 +29,10 @@ class TarefaAdapter() : RecyclerView.Adapter<TarefaAdapter.TarefaViewHolder>() {
         fun bind( tarefa: Tarefa){
             binding.textDescricao.text = tarefa.descricao
             binding.textData.text = tarefa.dataCadastro
+
+            binding.btnExcluir.setOnClickListener {
+                onClickExcluir( tarefa.idTarefa )
+            }
         }
     }
 
