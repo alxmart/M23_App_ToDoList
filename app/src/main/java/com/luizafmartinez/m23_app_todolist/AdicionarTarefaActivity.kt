0366b582454daff0891b3528986e75765dbf4a40
alxmart <alxmartinez@gmail.com>
2024-05-11 11:36:48 -0300

    Added ViewBinding config

--- app/src/main/java/com/luizafmartinez/m23_app_todolist/AdicionarTarefaActivity.kt
+++ app/src/main/java/com/luizafmartinez/m23_app_todolist/AdicionarTarefaActivity.kt
@@ -5,9 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.luizafmartinez.m23_app_todolist.databinding.ActivityAdicionarTarefaBinding
+import com.luizafmartinez.m23_app_todolist.databinding.ActivityMainBinding
 
 class AdicionarTarefaActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityAdicionarTarefaBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(binding.root)
     }
 }
